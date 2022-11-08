7d9040ebe275aeb8d8197ca34f5c1fdc7b00d0fd
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-11-07 18:35:53 -0700

    Saving my progress on the mock-challenge today. We got the schema up and running and created a few of the required functions.

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,7 @@
 class Freebie < ActiveRecord::Base
-
+    belongs_to :dev
+    belongs_to :company
+    def print_details
+        "#{dev.name} owns a #{name} from #{company.name}"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,8 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(name: "Keychain", value: 2, company_id: c1.id, dev_id: d2.id)
+Freebie.create(name: "Keychain", value: 2, company_id: Company.all.sample, dev_id: Dev.all.sample)
 
 puts "Seeding done!"
